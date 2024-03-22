aa0dae3bb06fbc9e4ee67dbda21412cc4557a322
OSIsrael <israelos067@gmail.com>
2024-03-22 08:32:03 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'OrellanaIsrael_In1A5',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
